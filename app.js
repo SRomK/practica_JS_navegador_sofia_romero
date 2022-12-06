f4aa28f328f7bc60cca2a9b0822c690e2da9d8f8
Sofia <75990610+SRomK@users.noreply.github.com>
2022-12-06 22:37:13 +0100

    html strcture and basic css

---
+++ app.js
@@ -0,0 +1,6 @@
+// se introduce un concepto y se agrega un li de ese concepto en el historial (tambien necesito que se puedan borrar)
+// y dependiendo de si es gasto se agrega un colorcito y otro para ingreso
+// y se muestra sumado arriba los gastos y los ingresos y arriba de todo tu ahorro (hacer las cuentas para esos elementos0)
+// mi AHORRO es la diferencia entre ingreso y gastos (ingresos - gastos)
+
+const createLi = document.querySelector("#li-accum");
